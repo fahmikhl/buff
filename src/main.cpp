966bffbae764380b2c1d598c016a135353ba7a10
fahmikhl <fahmikhoerullatif@gmail.com>
2020-02-29 18:59:12 +0700

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
-#include <Arduino.h>
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
 
 void setup() {
   // put your setup code here, to run once:
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
